1db7a6e03bdf7a5245c312fd1e5b1eae65d6391e
Simran Salunke <simransalunke9210@gmail.com>
2026-08-13 11:43:05 +0530

    Configure PostgreSQL for deployment

--- officemanegmenttracker/settings.py
+++ officemanegmenttracker/settings.py
@@ -100,11 +100,11 @@ WSGI_APPLICATION = 'officemanegmenttracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
 }
 
